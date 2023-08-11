7a9cc4bb70b6145ea3d947c5301e085515e5f8d6
Dung Nguyen <dungnguyen09172000@gmail.com>
2023-08-11 20:45:53 +0700

    Create Razor landing page

---
+++ razor-landing-page/README.md
@@ -0,0 +1,11 @@
+# React + Vite + TailwindCSS
+
+This template provides a minimal setup to get React working in Vite and TailwindCSS with HMR and some ESLint rules.
+
+## Information
+
+### Design
+
+![Razor Landing Page](http://url/to/img.png)
+
+[https://dribbble.com/shots/17800881-Web-UI](https://dribbble.com/shots/17800881-Web-UI)
---
+++ razor-landing-page/src/App.jsx
@@ -0,0 +1,28 @@
+function App() {
+  return (
+    <div className="min-h-screen w-full font-sans bg-gray-800 text-white">
+      <div>
+        <a href="#">Razor</a>
+        <ul>
+          <li>
+            <a href="#">Product</a>
+          </li>
+          <li>
+            <a href="#">Resources</a>
+          </li>
+          <li>
+            <a href="#">Support</a>
+          </li>
+          <li>
+            <a href="#">Pricing</a>
+          </li>
+          <li>
+            <a href="#">Blog</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
